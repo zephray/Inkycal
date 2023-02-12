36888476d24d6fc878bd1f4099bcfaea1b8bead0
Wenting Zhang <zephray@outlook.com>
2023-02-12 12:58:51 -0500

    Implement NOAA based weather plugin

--- inkycal/modules/__init__.py
+++ inkycal/modules/__init__.py
@@ -1,6 +1,7 @@
 from .inkycal_agenda import Agenda
 from .inkycal_calendar import Calendar
 from .inkycal_weather import Weather
+from .inkycal_noaaweather import NOAAWeather
 from .inkycal_feeds import Feeds
 from .inkycal_todoist import Todoist
 from .inkycal_image import Inkyimage
